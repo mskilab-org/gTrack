30ed18c4f0f64b44ee872d62d3ee98ca300abcb7
Jeremiah Wala <jwala@broadinstitute.org>
2016-03-15 12:51:12 -0400

    organizing code in gTrack

---
+++ tests/testthat/test_gTrack.R
@@ -0,0 +1,12 @@
+library(gTrack)
+context("gTrack tests")
+
+gr <- GRanges(c(1,2), IRanges(c(5,7), width=5), strand=c("+","-"))
+
+test_that("gTrack receives input vals into format", {
+
+  g <- gTrack(gr, legend = FALSE, height=7, xaxis.chronly = TRUE)
+  expect_equal(g$legend, FALSE)
+  expect_equal(g$height, 7)
+
+})
