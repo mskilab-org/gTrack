f9d31a641cdc518dc5d7187dec65b7cb5ac1e551
Jeremiah Wala <jwala@broadinstitute.org>
2016-03-17 23:47:06 -0400

    R CMD check pass?

--- tests/testthat/test_gTrack.R
+++ tests/testthat/test_gTrack.R
@@ -6,7 +6,7 @@ gr <- GRanges(c(1,2), IRanges(c(5,7), width=5), strand=c("+","-"))
 test_that("gTrack receives input vals into format", {
 
   g <- gTrack(gr, height=7, xaxis.chronly = TRUE)
-  expect_equal(g$legend, FALSE)
+  expect_equal(g$xaxis.chronly, TRUE)
   expect_equal(g$height, 7)
 
 })
