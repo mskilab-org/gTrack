9f4949ab2d268f48cade910a769691b370818d26
Jeremiah Wala <jwala@broadinstitute.org>
2016-03-21 12:04:20 -0400

    lots of bug fixes. integer-based plotting fix

--- tests/testthat/test_gTrack.R
+++ tests/testthat/test_gTrack.R
@@ -1,12 +1,26 @@
 library(gTrack)
+library(gUtils)
 context("gTrack tests")
 
-gr <- GRanges(c(1,2), IRanges(c(5,7), width=5), strand=c("+","-"))
+HI=300
+WI=600
+gr <- GRanges(c(1,1), IRanges(c(5,6), width=1), strand=c("+","-"), A=c(5,8), B=c(3,2), seqinfo= si)
+gr <- GRanges(c(1), IRanges(c(5), width=1), strand=c("+"), A=c(5), B=c(3), seqinfo= si)
+
+grl <- GRangesList(list(GRanges(c(1,2), IRanges(c(6,10), width=1), strand=c("+","-"),seqinfo=si)))
+mdat <- matrix(sample(10, 4, replace=TRUE), ncol=2, nrow=2)
+mdat[upper.tri(mdat)] <- mdat[lower.tri(mdat)]
 
 test_that("gTrack receives input vals into format", {
+  g <- gTrack(gr, height=7, xaxis.chronly = TRUE, sep.bg.col='red')
+  #png("rtdocs/figures/basic_background_col.png", height=HI, width=WI);
+  #gTrack::plot(g)
+  #dev.off()
 
-  g <- gTrack(gr, height=7, xaxis.chronly = TRUE)
-  expect_equal(g$xaxis.chronly, TRUE)
-  expect_equal(g$height, 7)
+  g <- gTrack(gr, xaxis.chronly = TRUE, mdata=mdat)
+  g2 <- gTrack(gr, height=7, xaxis.chronly = TRUE)
 
+  #png("rtdocs/figures/basic_background_col.png", height=HI, width=WI);
+  ##plot(g2, GRanges(1, IRanges(4,6)))
+  #dev.off()
 })
