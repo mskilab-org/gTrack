4992b0aa111c7a547d95accf1b022e396bb340ce
evanbiederstedt <evanbiederstedt@users.noreply.github.com>
2018-03-08 19:27:43 -0500

    duplicate alpha() removed

--- R/gTrack.R
+++ R/gTrack.R
@@ -5384,13 +5384,4 @@ listify = function(x, FUN, len = 1) {
     return(x)
 }
 
-alpha = function(col, alpha)
-{
-    col.rgb = col2rgb(col)
-    out = rgb(red = col.rgb['red', ]/255, green = col.rgb['green', ]/255, blue = col.rgb['blue', ]/255, alpha = alpha)
-    names(out) = names(col)
-    return(out)
-}
-
-
 
