72d52c71d806ca853fb6f27abfe1758759c37083
kevinmhadi <kevinmhadi.gmail.com>
2026-05-09 11:05:02 -0400

    Adding missing imports.

--- R/gTrack-package.R
+++ R/gTrack-package.R
@@ -1,10 +1,11 @@
 #' @import rtracklayer
 #' @import Rsamtools
 #' @importFrom bamUtils read.bam splice.cigar
-#' @importFrom data.table data.table rbindlist := setkeyv
-#' @importFrom GenomeInfoDb Seqinfo seqinfo keepSeqlevels seqlevels seqlengths seqlevels<- seqlengths<- genome<- seqnames
+#' @importFrom data.table data.table rbindlist `:=` setkeyv
+#' @importFrom GenomeInfoDb Seqinfo seqinfo keepSeqlevels seqlevels seqlengths seqlevels<- seqlengths<- genome<- seqnames genome
 #' @importFrom GenomicRanges GRanges values ranges width strand values<- strand<- seqnames coverage ranges<- reduce seqinfo
 #' @importFrom gUtils grl.unlist si2gr grbind gr.string gr.fix grl.pivot gr.findoverlaps gr.flatten gr.chr gr.match gr.sub
 #' @importFrom methods setClass setGeneric setMethod setRefClass
 #' @importFrom RCurl url.exists
+#' @importFrom IRanges "%over%"
 NULL
